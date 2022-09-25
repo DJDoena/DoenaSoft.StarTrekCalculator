e618647786e6efdb3bbc504660e5800733fbafae
DJ Doena <github@doena-soft.de>
2022-09-25 12:14:16 +0200

    1.0.17

---
+++ CalcTests/GetImage.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DoenaSoft.StarTrekCalculator.Tests
+{
+    [TestClass]
+    public sealed class GetImage
+    {
+        [TestMethod]
+        public void Warpchart()
+        {
+            using (var image = Images.GetWarpChart())
+            {
+                var fileName = Path.Combine(Path.GetTempPath(), "warpchart.jpg");
+
+                using (var fileStream = File.Create(fileName))
+                {
+                    var buffer = new byte[8192];
+
+                    int bytesRead;
+                    while ((bytesRead = image.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        fileStream.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DoenaSoft.StarTrekCalculator/Images.vb
@@ -0,0 +1,10 @@
+﻿Imports System.IO
+
+Public Module Images
+  Public Function GetWarpChart() As Stream
+    Dim result As Stream = GetType(Images).Assembly.GetManifestResourceStream("DoenaSoft.StarTrekCalculator.warpchart.jpg")
+
+    Return result
+  End Function
+
+End Module
